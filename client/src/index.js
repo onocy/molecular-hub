0e0436641cafeac92abf5465a457718f96dc011d
onocy <michalowski.phillip@gmail.com>
2019-06-30 02:50:48 -0400

    add log

--- client/src/index.js
+++ client/src/index.js
@@ -11,10 +11,11 @@ import App from './App';
 //Redux
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Provider } from 'react-redux'
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
 <Provider store={store}>
